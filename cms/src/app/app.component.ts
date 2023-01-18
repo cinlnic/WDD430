289a6f6af92d0d37d8201116321282f2fc8e783d
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-01-18 12:02:43 -0700

    cms updates

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cms';
+  selectedFeature: string = 'documents';
+
+  switchViews(selectedfeature: string) {
+    this.selectedFeature = selectedfeature;
+  }
 }
