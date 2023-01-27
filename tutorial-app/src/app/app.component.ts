4dd199aba3261dd0b717934ec2103b06b06d24d1
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-01-27 11:22:39 -0700

    Week 4

--- tutorial-app/src/app/app.component.ts
+++ tutorial-app/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {color: dodgerblue;}
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- title = 'tutorial-app';
+  numbers = [1, 2, 3, 4, 5];
+  oddNumber = [1, 3, 5];
+  onlyOdd = false;
 }
