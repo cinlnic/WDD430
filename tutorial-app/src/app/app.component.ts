00261b15c307af6a5bf19d66d2061bbbc8fc1b3e
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-02-13 11:22:55 -0700

    week 7 tutorials

--- tutorial-app/src/app/app.component.ts
+++ tutorial-app/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private ActivatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.ActivatedSub = this.userService.activatedEmiiter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.ActivatedSub.unsubscribe();
+  }
 }
