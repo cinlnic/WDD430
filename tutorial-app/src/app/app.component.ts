11b87b9bc52d376d695edf8cc055aee47bf1cf3d
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-02-21 14:46:05 -0700

    tutorial update

--- tutorial-app/src/app/app.component.ts
+++ tutorial-app/src/app/app.component.ts
@@ -1,25 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private ActivatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.ActivatedSub = this.userService.activatedEmiiter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    })
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 
-  ngOnDestroy(): void {
-    this.ActivatedSub.unsubscribe();
+  onSubmit(){
+    
   }
 }
